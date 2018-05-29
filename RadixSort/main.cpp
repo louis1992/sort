e5eeb4ec35a6da57f7020228c9402dfb9ff835bd
Baekdoohyun <whitehead2@Baekdoohyunui-iMac.local>
2018-05-29 22:30:51 +0900

    add file radix sort

--- RadixSort/main.cpp
+++ RadixSort/main.cpp
@@ -4,85 +4,72 @@
 
 using namespace std;
 
-void loadDate(vector<int>& source, int n);
-int getMax(vector<int>& source, int n);
-void countSort(vector<int>& source, int n, int exp);
-void radixSort(vector<int>& source, int n);
-void print(vector<int>& source, int n);
+void loadData(vector<int>& source);
+int getMax(vector<int>& source);
+void countSort(vector<int>& source, int exp);
+void radixSort(vector<int>& source);
+void print(vector<int>& source);
 
 int main(int argc, char** argv) {
     vector<int> in_data;
-    // LoadFile().
-    radixSort(in_data, in_data.size());
-    print(in_data, in_data.size());
+    loadData(in_data);
+    radixSort(in_data);
+    print(in_data);
     
     return 0;
 }
 
-void loadDate(vector<int>& source, int n) {
+void loadData(vector<int>& source) {
     // TODO.
+    ifstream inFile("random.txt");
+    int buf;
+    while (!inFile.eof()) {
+        inFile >> buf;
+        source.push_back(buf);
+    }
+    inFile.close();
 }
 
-int getMax(vector<int>& source, int n) {
+int getMax(vector<int>& source) {
     int max = source[0];
-    // TODO.
+    for (int itr = 0; itr < source.size(); ++itr) {
+        if (source[itr] > max)
+            max = source[itr];
+    }
     return max;
 }
 
-void countSort(vector<int>& source, int n, int exp) {
+void countSort(vector<int>& source, int exp) {
     // TODO.
-    vector<int> output;
     int count[10] = {0};
-
-    // Store count of occurrences in count[]
- 
-    // Change count[i] so that count[i] now contains actual
-    //  position of this digit in output[]
- 
-    // Build the output array
- 
-    // Copy the output array to arr[], so that arr[] now
-    // contains sorted numbers according to current digit
-}
-
-void radixSort(vector<int>& source, int n) {
-    // TODO.
-    int max = getMax(source, n);
+    int n = source.size();
+    int output[n];
     
-}
-
-void print(vector<int>& source, int n) {
-    for (int itr = 0; itr < n; ++itr)
-        cout << source[itr] << endl;
-}
-
-//////////////////////////////////////////////////////////
-/*
-void countSort(vector<int>& source, int n, int exp) {
-    // TODO.
-    vector<int> output;
-    int count[10] = {0};
-
-    // Store count of occurrences in count[]
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n; ++i) {
         count[(source[i]/exp)%10]++;
- 
-    // Change count[i] so that count[i] now contains actual
-    //  position of this digit in output[]
-    for (int i = 1; i < 10; i++)
-        count[i] += count[i - 1];
- 
-    // Build the output array
-    for (int i = n - 1; i >= 0; i--)
-    {
-        output[count[ (source[i]/exp)%10 ] - 1] = source[i];
-        count[ (source[i]/exp)%10 ]--;
     }
- 
-    // Copy the output array to arr[], so that arr[] now
-    // contains sorted numbers according to current digit
-    for (int i = 0; i < n; i++)
+
+    for (int i = 1; i < 10; ++i)
+        count[i] += count[i-1];
+    
+    for (int i = n-1; i >= 0; --i) {
+        output[count[(source[i]/exp)%10] -1] = source[i];
+        count[(source[i]/exp)%10]--;
+    }
+
+    for (int i = 0; i < n; ++i)
         source[i] = output[i];
 }
-*/
-//////////////////////////////////////////////////////////
+
+void radixSort(vector<int>& source) {
+    // TODO.
+    int max = getMax(source);
+    for (int exp = 1; max/exp > 0; exp *= 10) {
+        countSort(source, exp);
+    }
+}
+
+void print(vector<int>& source) {
+    for (int itr = 0; itr < source.size(); ++itr)
+        cout << source[itr] << endl;
+}
