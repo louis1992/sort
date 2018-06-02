42fa03a66aaca95bfa706444a392969d7eb80319
louis1992 <shinyk92@gmail.com>
2018-06-02 09:44:32 +0900

    Add skeleton code.

---
+++ BubbleSort/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+void loadData(vector<int>& source);
+void bubbleSort(vector<int>& source);
+void print(vector<int>& source);
+
+int main(int argc, char** argv) {
+    vector<int> in_data;
+    loadData(in_data);
+    bubbleSort(in_data);
+    print(in_data);
+    return 0;
+}
+
+void loadData(vector<int>& source) {
+    ifstream inFile("random.txt");
+    int buf;
+    while (!inFile.eof()) {
+        inFile >> buf;
+        source.push_back(buf);
+    }
+    inFile.close();
+}
+
+void bubbleSort(vector<int>& source) {
+    //TODO.
+}
+
+void print(vector<int>& source) {
+    for (int itr = 0; itr < source.size(); ++itr)
+        cout << source[itr] << endl;
+}
